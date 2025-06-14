e7541beec4772a2739d8f1bcd802aa803bbacc31
AtanuGpt <168507778+AtanuGpt@users.noreply.github.com>
2025-06-14 10:46:19 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import faiss
-import streamlit as st
-from dotenv import load_dotenv
-from langchain_core.messages import AIMessage, HumanMessage
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
-from llama_index.vector_stores.faiss import FaissVectorStore
-import azure.cognitiveservices.speech as speechsdk
