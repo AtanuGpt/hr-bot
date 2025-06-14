90ef96de1cc5ca3008e3a54d729b4b83aaab5990
AtanuGpt <168507778+AtanuGpt@users.noreply.github.com>
2025-06-14 10:16:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,92 @@
+import os
+import faiss
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, load_index_from_storage
+from llama_index.vector_stores.faiss import FaissVectorStore
+import azure.cognitiveservices.speech as speechsdk
+
+d = 1536
+faiss_index = faiss.IndexFlatL2(d)
+PERSIST_DIR = "./storage"
+
+load_dotenv()
+
+OPENAI_API_KEY=os.getenv("OPENAI_API_KEY")
+AZURE_SPEECH_KEY = os.getenv("AZURE_SPEECH_KEY")
+AZURE_SPEECH_ENDPOINT = os.getenv("AZURE_SPEECH_ENDPOINT")
+
+def fetchData(user_question):
+    try:
+        vector_store = FaissVectorStore.from_persist_dir("./storage")
+        storage_context = StorageContext.from_defaults(
+            vector_store=vector_store, persist_dir=PERSIST_DIR
+        )
+        index = load_index_from_storage(storage_context=storage_context)
+        query_engine = index.as_query_engine()
+        response = query_engine.query(user_question)
+        return str(response)
+    except:
+        return "Error"
+
+#============================================================================================================
+WelcomeMessage = """Hello, I am your HR Bot. I would try my best to answer your question from my knowledge-base"""
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content=WelcomeMessage)
+    ]
+
+speech_config = speechsdk.SpeechConfig(subscription=AZURE_SPEECH_KEY, endpoint=AZURE_SPEECH_ENDPOINT)
+speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"   
+speech_config.speech_synthesis_language = "en-US"
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+def main():
+
+    st.set_page_config(
+        page_title="Chat with HR Bot",
+        page_icon=":sparkles:"
+    )
+
+    st.header("Chat with HR Bot :sparkles:")   
+
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.markdown(message.content)     
+
+  
+    user_question = st.chat_input("Ask your question : ")
+
+    if user_question is not None and user_question != "":
+        st.session_state.chat_history.append(HumanMessage(content=user_question))
+
+        with st.chat_message("Human"):
+            st.markdown(user_question)
+        
+        with st.chat_message("AI"):
+            with st.spinner("Fetching data ..."):
+                response = fetchData(user_question)
+                st.markdown(response)    
+                
+        result = speech_synthesizer.speak_text_async(response).get()
+        st.session_state.chat_history.append(AIMessage(content=response))
+    
+    if "WelcomeMessage" not in st.session_state:
+        st.session_state.WelcomeMessage = WelcomeMessage
+        result = speech_synthesizer.speak_text_async(WelcomeMessage).get()
+
+#============================================================================================================
+if __name__ == '__main__':
+    main()
+
+
+
+
+
